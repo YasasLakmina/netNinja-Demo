dbfd9928bb0382de13b86b49ff55ae9ef20ca955
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-14 15:12:28 +0530

    commit1

--- server.js
+++ server.js
@@ -1,7 +1,21 @@
 const http = require("http");
+const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log("Request made..");
+  console.log("Request made..", req.url, req.method);
+
+  //set header content type
+  res.setHeader("Content-Type", "text/html");
+
+  //send an html file
+  fs.readFile("./views/index.html", (err, data) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, () => {
