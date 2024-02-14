d508605b9bbdb982520325e63f5c9779f20484cf
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-14 16:08:52 +0530

    copmmit3

--- server.js
+++ server.js
@@ -4,16 +4,39 @@ const fs = require("fs");
 const server = http.createServer((req, res) => {
   console.log("Request made..", req.url, req.method);
 
-  //set header content type
-  res.setHeader("Content-Type", "text/html");
+  //Small Routing System
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      //redirect the about-me to about page
+      res.setHeader("Location", "/about");
+      res.end();
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  //   //set header content type
+  //   res.setHeader("Content-Type", "text/html");
 
   //send an html file
-  fs.readFile("./views/index.html", (err, data) => {
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
-      res.write(data);
-      res.end();
+      //res.write(data);
+      res.end(data);
     }
   });
 });
