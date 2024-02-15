d7abd20819d345634579106848b459723505e110
Yasas Lakmina <138884690+YasasLakmina@users.noreply.github.com>
2024-02-15 12:28:36 +0530

    commit2

--- app.js
+++ app.js
@@ -1,11 +1,82 @@
 const express = require("express");
 const app = express();
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const Blog = require("./models/blog");
+
+const dbURI =
+  "mongodb+srv://YasasLakmina:Lakmina0115722795@cluster0.lbr7xtj.mongodb.net/Blog-Ninja?retryWrites=true&w=majority";
+
+mongoose
+  .connect(dbURI)
+  .then(() => app.listen(3000))
+  .catch((err) => console.log(err));
+
+//Adding to the database
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "New Blog 3",
+    snippet: "About My New Blog",
+    body: "More About My New Blog",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+//Retreving from database
+app.get("/all-blogs", (req, res) => {
+  Blog.find()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+//Retrieving Specific data from the database\
+app.get("/single-blog", (req, res) => {
+  Blog.findById("65cdaff4c005c71f68ea782e")
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+app.use(express.static("public"));
+app.use(morgan("tiny"));
+
+//Middleware Explanation
+
+// //1st middleware
+// app.use((req, res, next) => {
+//   console.log("new request made :");
+//   console.log("host :", req.hostname);
+//   console.log("path :", req.path);
+//   console.log("method :", req.method);
+
+//   //does not responsing then we have to use next functiont to move to the next middleware
+//   next();
+// });
+
+// //2nd middleware
+// app.use((req, res, next) => {
+//   console.log("in the next middleware");
+
+//   //does not responsing then we have to use next functiont to move to the next middleware
+//   next();
+// });
 
 //Register vire engine
 app.set("view engine", "ejs");
 
 //Listen for requests
-app.listen(3000);
 
 app.get("/", (req, res) => {
   const blogs = [
@@ -35,6 +106,7 @@ app.get("/blogs/create", (req, res) => {
 });
 
 //404 page
+//this is middleware function
 app.use((req, res) => {
   res.status(404).render("404", { title: "404" });
 });
